7f8e5425f6b09c22e244ac17055b8c43e98b7f22
Matthew Meyerink <meyerink@umich.edu>
2021-10-15 22:57:24 -0400

    Create Landing Page UI

--- DVY/ContentView.swift
+++ DVY/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isScanning: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.1, green: 0.1, blue: 0.1)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack() {
+                Text("Welcome to DVY")
+                    .font(.system(size: 45, weight: .semibold))
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 10)
+                
+                Text("Scan a recipt to start")
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 10)
+                
+                Button(action: {self.isScanning = true}) {
+                    Text("Scan")
+                }
+                    .buttonStyle(GreenButton())
+            }
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GreenButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 25, weight: .bold))
+            .padding(.vertical, 15)
+            .padding(.horizontal, 20)
+            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+            .background(Color(red: 0.2, green: 0.9, blue: 0.25))
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
