d4b285c75c1b4c579e697d3457bb06cf1198d26b
Matthew Meyerink <meyerink@umich.edu>
2021-10-15 21:24:54 -0400

    Initial Commit

---
+++ DVY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DVY
+//
+//  Created by Matthew Meyerink on 10/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DVY/DVYApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVYApp.swift
+//  DVY
+//
+//  Created by Matthew Meyerink on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DVYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
