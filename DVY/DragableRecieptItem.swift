2f3b48f7765dd3f6ceb1348c5b96fa2be2495776
Matthew Meyerink <meyerink@umich.edu>
2021-11-06 15:22:24 -0700

    Don't display price when item being dragged

--- DVY/DragableRecieptItem.swift
+++ DVY/DragableRecieptItem.swift
@@ -11,6 +11,7 @@ struct DragableRecieptItem: View {
     var item: ReciptItem
     
     @State var dragAmount = CGSize.zero
+    @State var maxWidth = CGFloat.infinity
     
     var body: some View {
         VStack {
@@ -18,11 +19,16 @@ struct DragableRecieptItem: View {
                 Text(item.name)
                     .font(.system(size: 20, weight: .semibold))
                     .padding(.leading, 5)
+                
                 Spacer()
-                Text(item.priceFormatted)
-                    .font(.system(size: 20, weight: .semibold))
-                    .padding(.trailing, 5)
+                
+                if (dragAmount == CGSize.zero) {
+                    Text(item.priceFormatted)
+                        .font(.system(size: 20, weight: .semibold))
+                        .padding(.trailing, 5)
+                }
             }
+                .frame(maxWidth: maxWidth)
         }
             .padding()
             .background(Color(red: 0.95, green: 0.8, blue: 0.5))
@@ -34,9 +40,11 @@ struct DragableRecieptItem: View {
                 DragGesture(coordinateSpace: .global)
                     .onChanged {
                         dragAmount = CGSize(width: $0.translation.width, height: $0.translation.height)
+                        maxWidth = 100
                     }
                     .onEnded { _ in
-                       dragAmount = .zero
+                        dragAmount = .zero
+                        maxWidth = .infinity
                     }
             )
     }
