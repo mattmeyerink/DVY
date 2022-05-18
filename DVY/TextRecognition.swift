cdaac5a8f6878bba087bf985e2feb60bd73fdf43
mattmeyerink <meyerink@umich.edu>
2022-05-17 22:00:14 -0400

    refactor text recognition logic

--- DVY/TextRecognition.swift
+++ DVY/TextRecognition.swift
@@ -17,54 +17,7 @@ class TextRecognitionResponse {
 func recognizeText(from images: [CGImage]) -> TextRecognitionResponse {
     let response = TextRecognitionResponse()
     
-    let recognizeTextRequest = VNRecognizeTextRequest { (request, error) in
-        guard error == nil else { return }
-        
-        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
-        
-        let maximumRecognitionCandidates = 1
-        
-        var currentItemName: String? = nil
-        var currentItemPrice: Double? = nil
-        var currentTax: Double = 0.0
-        let invalidItems = ["total", "due", "visa"]
-        
-        for observation in observations {
-            let candidate = observation.topCandidates(maximumRecognitionCandidates).first
-            
-            if (candidate != nil) {
-                var updatedString = candidate!.string.replacingOccurrences(of: "$", with: "")
-                updatedString = updatedString.replacingOccurrences(of: ",", with: ".")
-                
-                currentItemPrice = Double(updatedString)
-                
-                if (currentItemName != nil && currentItemPrice != nil && candidate!.string.contains(".") && currentItemPrice != 0) {
-                    var containsInvalidWord = false
-                    
-                    for word in invalidItems {
-                        if (currentItemName!.localizedCaseInsensitiveContains(word)) {
-                            containsInvalidWord = true
-                            break
-                        }
-                    }
-                    
-                    let containsTax = currentItemName!.localizedCaseInsensitiveContains("tax")
-                    
-                    if (containsTax && currentItemPrice! > currentTax) {
-                        currentTax = currentItemPrice!
-                        response.tax = CurrencyObject(price: currentTax)
-                    } else if (!containsInvalidWord && !containsTax) {
-                        response.items.append(ReciptItem(name: currentItemName!, price: currentItemPrice!))
-                    }
-                    
-                    currentItemName = nil
-                    currentItemPrice = nil
-                } else {
-                    currentItemName = candidate!.string
-                }
-            }
-        }
-    }
+    let recognizeTextRequest = createRecognitionRequest(response: response)
     
     recognizeTextRequest.recognitionLevel = .accurate
     
@@ -74,4 +27,71 @@ func recognizeText(from images: [CGImage]) -> TextRecognitionResponse {
     }
     
     return response
+}
+
+func createRecognitionRequest(response: TextRecognitionResponse) -> VNRecognizeTextRequest {
+    return VNRecognizeTextRequest { (request, error) in
+        guard error == nil else { return }
+        
+        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+        
+        let maximumRecognitionCandidates = 1
+        
+        var currentItemName: String? = nil
+        var currentItemPrice: Double? = nil
+        var currentTax: Double = 0.0
+        
+        for observation in observations {
+            let candidate = observation.topCandidates(maximumRecognitionCandidates).first
+            if (candidate == nil) { continue }
+            
+            currentItemPrice = formatStringToPrice(scannedString: candidate!.string)
+            
+            if (isCompleteItem(name: currentItemName, price: currentItemPrice, rawText: candidate!.string)) {
+                let containsTax = currentItemName!.localizedCaseInsensitiveContains("tax")
+                
+                if (containsTax && currentItemPrice! > currentTax) {
+                    currentTax = currentItemPrice!
+                    response.tax = CurrencyObject(price: currentTax)
+                } else if (!containsTax && !nameContainsInvalidWord(name: currentItemName)) {
+                    response.items.append(ReciptItem(name: currentItemName!, price: currentItemPrice!))
+                }
+                
+                currentItemName = nil
+                currentItemPrice = nil
+            } else {
+                currentItemName = candidate!.string
+            }
+        }
+    }
+}
+
+func formatStringToPrice(scannedString: String) -> Double? {
+    var formattedString = scannedString
+    
+    formattedString = formattedString.replacingOccurrences(of: "$", with: "")
+    formattedString = formattedString.replacingOccurrences(of: ",", with: ".")
+    
+    return Double(formattedString)
+}
+
+func isCompleteItem(name: String?, price: Double?, rawText: String) -> Bool {
+        let itemNameValid = name != nil
+        let itemPriceValid = price != nil && price != 0 && rawText.contains(".")
+    
+        return itemNameValid && itemPriceValid
+}
+
+func nameContainsInvalidWord(name: String?) -> Bool {
+    var nameIsInvalid = false
+    let invalidItems = ["total", "due", "visa"]
+    
+    for word in invalidItems {
+        if (name!.localizedCaseInsensitiveContains(word)) {
+            nameIsInvalid = true
+            break
+        }
+    }
+    
+    return nameIsInvalid
 }
