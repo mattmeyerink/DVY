e0bb75c504639686d87f39f7bb276096973b9486
mattmeyerink <meyerink@umich.edu>
2022-07-22 21:56:30 -0400

    visual improvements to confirmation flow

--- DVY/ScanConfirmation/ConfirmationFlow/ConfirmationFlowIntro.swift
+++ DVY/ScanConfirmation/ConfirmationFlow/ConfirmationFlowIntro.swift
@@ -13,11 +13,55 @@ struct ConfirmationFlowIntro: View {
     
     var body: some View {
         VStack {
-            Text("This is a flow to correct and confirm the items from the receipt. When you click continue, items scanned from the receipt will be displayed one by one in order. At each item you can delete the item, edit the item, or insert a missed item before selecting next to continue. At the end, confirm you want to accept the new item count and total.")
-                .font(.system(size: 20, weight: .regular))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.white)
-                .padding(.horizontal)
+            Group {
+                Text("This is a flow to correct the items scanned from the receipt.")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text("Items from the receipt will be displayed in order.")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+            }
+        
+            Spacer()
+            
+            Group {
+                Text("At each item you can...")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text("1. Edit the price/name of the current item.")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text("2. Insert an item if one was missed.")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text("3. Delete an item if it is not needed.")
+                    .font(.system(size: 20, weight: .regular))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+            }
             
             Spacer()
             
