12c7ae23419f96ed03d47e46e0ea28eaff69a2de
mattmeyerink <meyerink@umich.edu>
2022-07-30 23:06:16 -0400

    refactor previously added friends to component

--- DVY/Friends/AddFriendsPage.swift
+++ DVY/Friends/AddFriendsPage.swift
@@ -77,39 +77,11 @@ struct AddFriendsPage: View {
                 }
                 
                 if (getArePreviouslyAddedFriendsVisible()) {
-                    Text("Previous Friends")
-                        .font(.system(size: 30, weight: .semibold))
-                        .padding(.vertical, 15)
-                        .foregroundColor(Color.white)
-                    
-                    ScrollView {
-                        ForEach(previouslyAddedFriends.indices, id: \.self) { i in
-                            if (!Set(friends.map { $0.id }).contains(previouslyAddedFriends[i].id)) {
-                                VStack {
-                                    HStack {
-                                        Text(previouslyAddedFriends[i].firstName + " " + previouslyAddedFriends[i].lastName)
-                                            .font(.system(size: 20, weight: .semibold))
-                                            .padding(.leading, 5)
-                                        
-                                        Spacer()
-                                    }
-                                }
-                                    .padding()
-                                    .background(
-                                        Color(
-                                            red: previouslyAddedFriends[i].color.red,
-                                            green: previouslyAddedFriends[i].color.green,
-                                            blue: previouslyAddedFriends[i].color.blue
-                                        )
-                                    )
-                                    .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
-                                    .cornerRadius(10)
-                                    .onTapGesture {
-                                        openPreviouslySelectedFriendModal(currentFriend: previouslyAddedFriends[i])
-                                    }
-                            }
-                        }
-                    }
+                    PreviouslyAddedFriendsPage(
+                        friends: $friends,
+                        previouslyAddedFriends: $previouslyAddedFriends,
+                        openPreviouslySelectedFriendModal: openPreviouslySelectedFriendModal
+                    )
                 }
             }
                 .padding(.horizontal)
---
+++ DVY/Friends/PreviouslyAddedFriendsPage.swift
@@ -0,0 +1,51 @@
+//
+//  PreviouslyAddedFriendsPage.swift
+//  DVY
+//
+//  Created by Matthew Meyerink on 7/30/22.
+//
+
+import SwiftUI
+
+struct PreviouslyAddedFriendsPage: View {
+    @Binding var friends: [Person]
+    @Binding var previouslyAddedFriends: [Person]
+    
+    @State var openPreviouslySelectedFriendModal: (Person) -> Void
+    
+    var body: some View {
+        Text("Previous Friends")
+            .font(.system(size: 30, weight: .semibold))
+            .padding(.vertical, 15)
+            .foregroundColor(Color.white)
+        
+        ScrollView {
+            ForEach(previouslyAddedFriends.indices, id: \.self) { i in
+                if (!Set(friends.map { $0.id }).contains(previouslyAddedFriends[i].id)) {
+                    VStack {
+                        HStack {
+                            Text(previouslyAddedFriends[i].firstName + " " + previouslyAddedFriends[i].lastName)
+                                .font(.system(size: 20, weight: .semibold))
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                        }
+                    }
+                        .padding()
+                        .background(
+                            Color(
+                                red: previouslyAddedFriends[i].color.red,
+                                green: previouslyAddedFriends[i].color.green,
+                                blue: previouslyAddedFriends[i].color.blue
+                            )
+                        )
+                        .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            openPreviouslySelectedFriendModal(previouslyAddedFriends[i])
+                        }
+                }
+            }
+        }
+    }
+}
