3c792e94e6c2f570b23c04c78812eff645e9bae1
mattmeyerink <meyerink@umich.edu>
2022-08-01 22:00:16 -0700

    add search box and filter logic to previously added friends

--- DVY/Friends/PreviouslyAddedFriendsPage.swift
+++ DVY/Friends/PreviouslyAddedFriendsPage.swift
@@ -6,18 +6,26 @@
 //
 
 import SwiftUI
+import Combine
 
 struct PreviouslyAddedFriendsPage: View {
     @Binding var friends: [Person]
     @Binding var previouslyAddedFriends: [Person]
     
     @State var openPreviouslySelectedFriendModal: (Person) -> Void
+    @State var searchText: String = ""
+    @State var filteredPreviouslyAddedFriends: [Person] = []
     
     var body: some View {
         Text("Previous Friends")
             .font(.system(size: 30, weight: .semibold))
             .padding(.vertical, 15)
             .foregroundColor(Color.white)
+        
+        TextField("Search...", text: $searchText)
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            .onReceive(Just(searchText), perform: updateFilteredFriendList)
         
         ScrollView {
             ForEach(previouslyAddedFriends.indices, id: \.self) { i in
@@ -47,5 +55,21 @@ struct PreviouslyAddedFriendsPage: View {
                 }
             }
         }
+    }
+    
+    func updateFilteredFriendList(newSearchTerm: String) -> Void {
+        if (searchText != "") {
+            filteredPreviouslyAddedFriends = previouslyAddedFriends.filter { personMatchesSearchTerm(searchTerm: newSearchTerm, friend: $0) }
+        } else {
+            filteredPreviouslyAddedFriends = previouslyAddedFriends
+        }
+        
+        print(filteredPreviouslyAddedFriends)
+    }
+    
+    func personMatchesSearchTerm(searchTerm: String, friend: Person) -> Bool {
+        let friendNameFormatted = (friend.firstName + friend.lastName).lowercased().replacingOccurrences(of: " ", with: "")
+        let searchTermFormatted = searchTerm.lowercased().replacingOccurrences(of: " ", with: "")
+        return friendNameFormatted.contains(searchTermFormatted)
     }
 }
